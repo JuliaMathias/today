ffc9d29d9e1fbb9d26212f72f20f866ff0720849
JuliaMathias <juliamss@gmail.com>
2022-03-22 14:06:14 -0300

    refactor: generate one twitter link isntead of a list

--- lib/today/links.ex
+++ lib/today/links.ex
@@ -4,24 +4,20 @@ defmodule Today.Links do
   """
 
   @doc """
-    Generates a list of twitter search links for a given account and year range.
+    Generates a list of tuples with the year and a twitter search link for a given account and year.
 
   ### Example
 
-  iex> generate_twitter_links("thejuliams", ["2010", "2011"])
-  [
-    "https://twitter.com/search?q=from%3Athejuliams%20since%3A2010-03-17%20until%3A2010-03-18&src=recent_search_click",
-    "https://twitter.com/search?q=from%3Athejuliams%20since%3A2011-03-17%20until%3A2011-03-18&src=recent_search_click"
-  ]
+  iex> generate_twitter_links("thejuliams", "2010")
+  "https://twitter.com/search?q=from%3Athejuliams%20since%3A2010-03-17%20until%3A2010-03-18&src=recent_search_click"
+
 
   """
-  @spec generate_twitter_links(String.t(), range :: [String.t()]) :: list
-  def generate_twitter_links(account, range) do
+  @spec generate_twitter_link(String.t(), range :: String.t()) :: String.t()
+  def generate_twitter_link(account, year) do
     today = Date.utc_today()
     tomorrow = Date.utc_today() |> Date.add(1)
 
-    Enum.map(range, fn year ->
-      "https://twitter.com/search?q=from%3A#{account}%20since%3A#{year}-#{today.month}-#{today.day}%20until%3A#{year}-#{tomorrow.month}-#{tomorrow.day}&src=recent_search_click"
-    end)
+    "https://twitter.com/search?q=from%3A#{account}%20since%3A#{year}-#{today.month}-#{today.day}%20until%3A#{year}-#{tomorrow.month}-#{tomorrow.day}&src=recent_search_click"
   end
 end
